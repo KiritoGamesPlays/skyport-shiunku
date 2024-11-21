ecb58cea46b7054cd108e2198d834ac9742fc829
ShindoZk <77684277+shindozk@users.noreply.github.com>
2024-11-21 02:42:55 +0000

    fix seed

--- skyport/exec/seed.js
+++ skyport/exec/seed.js
@@ -36,7 +36,7 @@ async function seed() {
 
 async function performSeeding() {
   try {
-    const imagesIndexResponse = await axios.get('https://raw.githubusercontent.com/shindozk/skyport-shiunku/main/images/seed/seed_skyport_latest.json');
+    const imagesIndexResponse = await axios.get('https://raw.githubusercontent.com/shindozk/skyport-shiunku-images/main/seed/seed_skyport_latest.json');
     const imageUrls = imagesIndexResponse.data;
     let imageDataArray = [];
 
